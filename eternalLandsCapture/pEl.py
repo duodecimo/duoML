efc0807738d462ffbefaa7ccf2942fdd48e376e2
duodecimo <duodecimo@gmail.com>
2018-06-27 10:31:01 -0300

    adjusting capture points

--- eternalLandsCapture/pEl.py
+++ eternalLandsCapture/pEl.py
@@ -4,35 +4,39 @@ import os
 
 os.system('shutter -n -c -e --disable_systray -o elCapt.png --window=.*Joachim.*')
 
-# Load an color image in color without alpha (for bw use 0, for alpha color use -1)
+# Load an color image in color without alpha (for gray use 0, for alpha color use -1)
 img = cv2.imread('elCapt.png',1)
 height, width, _ = img.shape
-harvHeight = int(0.97 * height)
-harvWidth = int(0.9 * width)
-harvSize = int(0.033 * height)
 
 imgB = cv2.imread('elCapt.png',1)
-healthWidth = int(0.188 * width)
-foodWidth = int(0.1 * width)
-loadWidth = int(0.28 * width)
-hflHeight = int(0.93 * height)
-hflSize = int(0.033 * height)
 
-#imgHarv = img[840:840+36, 1388:1388+28]
-#imgHarv = img[harvHeight:harvHeight+28, harvWidth:harvWidth+28]
-imgHarv = img[harvHeight:harvHeight+harvSize, harvWidth:harvWidth+harvSize]
-imgHealth = imgB[hflHeight:hflHeight+hflSize, healthWidth:healthWidth+hflSize]
-imgFood = imgB[hflHeight:hflHeight+hflSize, foodWidth:foodWidth+hflSize]
-imgLoad = imgB[hflHeight:hflHeight+hflSize, loadWidth:loadWidth+hflSize]
-cv2.imshow('image',imgHarv)
-cv2.imshow('image1',imgHealth)
-cv2.imshow('image2',imgFood)
-cv2.imshow('image3',imgLoad)
+################
+# orinal size 1545 x 866
+# percentages
+vheight = int((13 * 100 / 866) / 100  * height)
+vwidth = int((28 * 100 / 1545) / 100 * width)
+print('height: ', vheight, ' width: ', vwidth)
+# origins
+wipfood = int((148 * 100 / 1545) / 100 * width)
+wiphealth = int((289 * 100 / 1545) / 100 * width)
+wipload = int((430 * 100 / 1545) / 100 * width)
+hip = int((816 * 100 / 866) / 100 * height)
+print('origins: food(', hip, ',', wipfood, ') health(', hip, ',', wiphealth, ') load(', hip, ',', wipload, ')')
+imgFood = imgB[hip:hip+vheight, wipfood:wipfood+vwidth]
+imgHealth = imgB[hip:hip+vheight, wiphealth:wiphealth+vwidth]
+imgLoad = imgB[hip:hip+vheight, wipload:wipload+vwidth]
+#imgHarv = img[?, ?]
+################
+
+#cv2.imshow('image',imgHarv)
+cv2.imshow('food',imgFood)
+cv2.imshow('health',imgHealth)
+cv2.imshow('load',imgLoad)
 k = cv2.waitKey(0) & 0xFF
 if k == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
 elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('elHarvesting.png',imgHarv)
+    #cv2.imwrite('elHarvesting.png',imgHarv)
     cv2.imwrite('elHealth.png',imgHealth)
     cv2.imwrite('elFood.png',imgFood)
     cv2.imwrite('elLoad.png',imgLoad)
