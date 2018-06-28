8769fcaa95dc5eccefc45221ef7c6bdf4e438e51
duodecimo <duodecimo@gmail.com>
2018-06-28 11:47:30 -0300

    +

--- eternalLandsCapture/digitRecognition/performRecognition.py
+++ eternalLandsCapture/digitRecognition/performRecognition.py
@@ -9,16 +9,17 @@ clf = joblib.load("digits_cls.pkl")
 
 # Read the input image 
 #im = cv2.imread("photo_2.jpg")
-im = cv2.imread("elLoad.png")
+im = cv2.imread("elHealth.png")
+#im = cv2.imread("Figure_duo.png")
 
 # Convert to grayscale and apply Gaussian filtering
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+#im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
 # Threshold the image
 #ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
-#ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY)
-ret, im_th = cv2.threshold(im_gray, 75, 255, cv2.THRESH_BINARY)
+ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY)
+#ret, im_th = cv2.threshold(im_gray, 75, 255, cv2.THRESH_BINARY_INV)
 
 cv2.imshow('image1',im_gray)
 cv2.imshow('image2',im_th)
@@ -27,10 +28,9 @@ k = cv2.waitKey(0) & 0xFF
 # Find contours in the image
 #ctrs, hier, _ = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 #im2, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+im2, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-im2, ctrs, hier = cv2.findContours(im_gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-print('contours: ', ctrs)
+#print('contours: ', ctrs)
 
 # Get rectangles contains each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
@@ -38,23 +38,26 @@ rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
 for rect in rects:
-    # Draw the rectangles
-    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
-    # Make the rectangular region around the digit
     leng = int(rect[3] * 1.6)
-    pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-    pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-    print('pt1: ', pt1, 'leng: ', leng, 'pt2: ', pt2)
-    roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
-    # Resize the image
-    roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-    roi = cv2.dilate(roi, (3, 3))
-    # Calculate the HOG features
-    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-    nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
-    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    if leng > 70:
+      # Draw the rectangles
+      cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+      # Make the rectangular region around the digit
     
-    print('nbr: ', int(nbr[0]))
+      pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
+      pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
+      print('pt1: ', pt1, 'leng: ', leng, 'pt2: ', pt2)
+      roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+      # Resize the image
+      roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+      roi = cv2.dilate(roi, (3, 3))
+      # Calculate the HOG features
+      roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+      nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+      cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
+    
+    
+      print('nbr: ', int(nbr[0]))
 
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey() & 0xFF
--- eternalLandsCapture/pEl.py
+++ eternalLandsCapture/pEl.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 import os
 from matplotlib import pyplot as plt
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
 
 #os.system('shutter -n -c -e --disable_systray -o elCapt.png --window=.*Joachim.*')
 
@@ -23,27 +26,45 @@ wiphealth = int((289 * 100 / 1545) / 100 * width)
 wipload = int((430 * 100 / 1545) / 100 * width)
 hip = int((818 * 100 / 866) / 100 * height)
 print('origins: food(', hip, ',', wipfood, ') health(', hip, ',', wiphealth, ') load(', hip, ',', wipload, ')')
-imgFood = imgB[hip:hip+vheight, wipfood:wipfood+vwidth]
-imgHealth = imgB[hip:hip+vheight, wiphealth:wiphealth+vwidth]
-imgLoad = imgB[hip:hip+vheight, wipload:wipload+vwidth]
+imgFood = img[hip:hip+vheight, wipfood:wipfood+vwidth]
+imgHealth = img[hip:hip+vheight, wiphealth:wiphealth+vwidth]
+imgLoad = img[hip:hip+vheight, wipload:wipload+vwidth]
+bimgFood = imgB[hip:hip+vheight, wipfood:wipfood+vwidth]
+bimgHealth = imgB[hip:hip+vheight, wiphealth:wiphealth+vwidth]
+bimgLoad = imgB[hip:hip+vheight, wipload:wipload+vwidth]
 #imgHarv = img[?, ?]
 ################
 
-ret,imgFood = cv2.threshold(imgFood, 75, 255, cv2.THRESH_BINARY_INV)
-ret,imgHealth = cv2.threshold(imgHealth, 75, 255, cv2.THRESH_BINARY_INV)
-ret,imgLoad = cv2.threshold(imgLoad, 75, 255, cv2.THRESH_BINARY_INV)
+_,cimgFood = cv2.threshold(bimgFood, 75, 255, cv2.THRESH_BINARY)
+_,cimgHealth = cv2.threshold(bimgHealth, 75, 255, cv2.THRESH_BINARY)
+_,cimgLoad = cv2.threshold(bimgLoad, 75, 255, cv2.THRESH_BINARY)
 
-mask = 255 * np.ones((60, 60), np.uint8)
+mask = 255 * np.ones((60, 60, 3), np.uint8)
 mask[18:18+13, 12:12+28] = imgFood
-imgFood = cv2.resize(mask, (1200, 1200), interpolation = cv2.INTER_AREA)
+imgFood = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
 
-mask = 255 * np.ones((60, 60), np.uint8)
+mask = np.zeros((60, 60), np.uint8)
+mask[18:18+13, 12:12+28] = cimgFood
+cimgFood = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
+
+mask = 255 * np.ones((60, 60, 3), np.uint8)
 mask[18:18+13, 12:12+28] = imgHealth
-imgHealth = cv2.resize(mask, (1200, 1200), interpolation = cv2.INTER_AREA)
+imgHealth = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
 
-mask = 255 * np.ones((60, 60), np.uint8)
+mask = np.zeros((60, 60), np.uint8)
+mask[18:18+13, 12:12+28] = cimgHealth
+cimgHealth = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
+
+mask = 255 * np.ones((60, 60, 3), np.uint8)
 mask[18:18+13, 12:12+28] = imgLoad
-imgLoad = cv2.resize(mask, (1200, 1200), interpolation = cv2.INTER_AREA)
+imgLoad = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
+
+mask = 255 * np.zeros((60, 60), np.uint8)
+mask[18:18+13, 12:12+28] = cimgLoad
+cimgLoad = cv2.resize(mask, (120, 120), interpolation = cv2.INTER_AREA)
+
+_, contours, _ = cv2.findContours(cimgLoad, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(imgLoad, contours, -1, (0,255,0), 1)
 
 #cv2.imshow('image',imgHarv)
 titles = ['food', 'health', 'load']
@@ -58,24 +79,6 @@ plt.show()
 cv2.imwrite('elHealth.png',imgHealth)
 cv2.imwrite('elFood.png',imgFood)
 cv2.imwrite('elLoad.png',imgLoad)
-
-
-# find contours in the thresholded image, then initialize the
-# digit contours lists
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-digitCnts = []
- 
-# loop over the digit area candidates
-for c in cnts:
-# compute the bounding box of the contour
-(x, y, w, h) = cv2.boundingRect(c)
- 
-# if the contour is sufficiently large, it must be a digit
-if w >= 15 and (h >= 30 and h <= 40):
-digitCnts.append(c)
-
 
 #k = cv2.waitKey(0) & 0xFF
 #if k == 27:         # wait for ESC key to exit
